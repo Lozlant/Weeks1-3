f75d5f593a6ff36ff7c9020fa7dc6d7a2c6929c7
Lozlant <Lozlant@outlook.com>
2025-01-15 11:06:28 -0500

    Done the tank challenge

---
+++ Assets/Scripts/Barrel.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos=Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction=mousePos-(Vector2)transform.position;
+        transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
+    }
+}
